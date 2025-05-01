545e348075062f40be1a4a3dc17b8a11e7a171a4
JAulds <jacksonaulds@gmail.com>
2025-05-01 22:51:42 +0500

    Update CustomTextInput.js
    Revamp the custom text input

--- components/CustomTextInput.js
+++ components/CustomTextInput.js
@@ -1,89 +1,64 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { TextInput, View, Text, StyleSheet } from "react-native";
-
-class CustomTextInput extends Component {
-  render() {
-    const {
-      label,
-      labelStyle,
-      maxLength,
-      textInputStyle,
-      stateHolder,
-      stateFieldName,
-      onChangeText, // New prop for custom change handler
-      error, // New prop for error message
-      ...props // Capture any other props
-    } = this.props;
-
-    return (
-      <View style={{ marginBottom: 10 }}>
-        <Text style={[styles.fieldLabel, labelStyle]}>{label}</Text>
-        <TextInput
-          maxLength={maxLength}
-          onChangeText={(inText) => {
-            // Update state through stateHolder
-            stateHolder.setState(() => {
-              const obj = {};
-              obj[stateFieldName] = inText;
-              return obj;
-            });
-            // Call custom onChangeText if provided
-            if (onChangeText) {
-              onChangeText(inText);
-            }
-          }}
-          style={[
-            styles.textInput,
-            textInputStyle,
-            error ? { borderColor: 'red', borderWidth: 1 } : {}
-          ]}
-          {...props} // Spread any additional props
-        />
-        {error && (
-          <Text style={{ color: 'red', marginLeft: 10, fontSize: 12 }}>
-            {error}
-          </Text>
-        )}
-      </View>
-    );
-  }
-}
-
-CustomTextInput.propTypes = {
-  label: PropTypes.string.isRequired,
-  labelStyle: PropTypes.object,
-  maxLength: PropTypes.number,
-  textInputStyle: PropTypes.object,
-  stateHolder: PropTypes.object.isRequired,
-  stateFieldName: PropTypes.string.isRequired,
-  onChangeText: PropTypes.func, // New prop type
-  error: PropTypes.string // New prop type for error message
-};
-
-
-CustomTextInput.defaultProps = {
-  placeholder: "",
-  secureTextEntry: false,
-  keyboardType: "default",
-};
-
+import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+/**
+* Styles used by this component.
+*/
 const styles = StyleSheet.create({
-  container: {
-    marginBottom: 16,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingHorizontal: 8,
-    borderRadius: 4,
-    backgroundColor: "#fff",
-  },
+    fieldLabel : {
+        marginLeft : 10
+    },
+    textInput : {
+        height : 40, marginLeft : 10, width : "96%", marginBottom : 20,
+        /* Branch on platform type for different styling. */
+        ...Platform.select({
+            ios : {
+            marginTop : 4, paddingLeft : 10, borderRadius : 8,
+            borderColor : "#c0c0c0", borderWidth : 2
+            },
+            android : { }
+        })
+    }
 });
+/**
+* Define a custom TextInput component.
+*/
+class CustomTextInput extends Component {
+    /**
+    * Render this component.
+    */
+    render() {
+        const {
+        label, labelStyle, maxLength, textInputStyle, stateHolder, stateFieldName
+        } = this.props;
+        return (
+            <View>
+                <Text style={ [ styles.fieldLabel, labelStyle ] }>{label}</Text>
+                <TextInput
+                    maxLength={ maxLength }
+                    onChangeText={ (inText) => stateHolder.setState(
+                    () => {
+                    const obj = { };
+                    obj[stateFieldName] = inText;
+                    return obj;
+                    }
+                    ) }
+                    style={ [ styles.textInput, textInputStyle ] }
+                />
+            </View>
+        );
+    }
+} /* End customTextInput component. */
+/**
+* Define properties this component will support.
+*/
+CustomTextInput.propTypes = {
+    label : PropTypes.string.isRequired,
+    labelStyle : PropTypes.object,
+    maxLength : PropTypes.number,
+    textInputStyle : PropTypes.object,
+    stateHolder : PropTypes.object.isRequired,
+    stateFieldName : PropTypes.string.isRequired
+}; /* End propTypes. */
 
 export default CustomTextInput;
