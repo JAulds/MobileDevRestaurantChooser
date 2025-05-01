f00c4fa4f62c48c6d37faa7865ca4a083e959a1a
JAulds <jacksonaulds@gmail.com>
2025-05-01 22:54:52 +0500

    Use prettier
    Adjust the readability of code

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,74 +1,115 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from "react-native";
-
-export default function ListScreen({ restaurants, deleteRestaurant }) {
-  const renderItem = ({ item }) => (
-    <View style={styles.restaurantItem}>
-      <View>
-        <Text style={styles.restaurantName}>{item.name}</Text>
-        <Text style={styles.restaurantDetails}>
-          Cuisine: {item.cuisine}, Rating: {item.starRating} stars, Price: {item.priceRating}
-        </Text>
-        <Text style={styles.restaurantDetails}>
-          Phone: {item.phone}, Address: {item.address}
-        </Text>
-      </View>
-      <TouchableOpacity
-        style={styles.deleteButton}
-        onPress={() => deleteRestaurant(item.id)}
-      >
-        <Text style={styles.deleteButtonText}>Delete</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Restaurant List</Text>
-      <FlatList
-        data={restaurants}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#f8f8f8",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  restaurantItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 8,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-  },
-  restaurantName: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  restaurantDetails: {
-    fontSize: 14,
-    color: "gray",
-  },
-  deleteButton: {
-    backgroundColor: "#ff4d4d",
-    paddingVertical: 4,
-    paddingHorizontal: 12,
-    borderRadius: 4,
-  },
-  deleteButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-});
+class ListScreen extends React.Component {
+  /**
+   * Constructor.
+   */
+  constructor(inProps) {
+    super(inProps);
+    this.state = {
+      listData: [],
+    };
+  } /* End constructor. */
+  /**
+   * Render this component.
+   */
+  render() {
+    return (
+      <Root>
+        <View style={styles.listScreenContainer}>
+          {/* ########## Add Restaurant button ########## */}
+          <CustomButton
+            text="Add Restaurant"
+            width="94%"
+            onPress={() => {
+              this.props.navigation.navigate("AddScreen");
+            }}
+          />
+          {/* ########## Restaurant list ########## */}
+          <FlatList
+            style={styles.restaurantList}
+            data={this.state.listData}
+            renderItem={({ item }) => (
+              <View style={styles.restaurantContainer}>
+                <Text style={styles.restaurantName}>{item.name}</Text>
+                <CustomButton
+                  text="Delete"
+                  onPress={() => {
+                    Alert.alert(
+                      "Please confirm",
+                      "Are you sure you want to delete this restaurant?",
+                      [
+                        {
+                          text: "Yes",
+                          onPress: () => {
+                            // Pull data out of storage.
+                            AsyncStorage.getItem(
+                              "restaurants",
+                              function (inError, inRestaurants) {
+                                if (inRestaurants === null) {
+                                  inRestaurants = [];
+                                } else {
+                                  inRestaurants = JSON.parse(inRestaurants);
+                                }
+                                // Find the right one to delete and splice it out.
+                                for (let i = 0; i < inRestaurants.length; i++) {
+                                  const restaurant = inRestaurants[i];
+                                  if (restaurant.key === item.key) {
+                                    inRestaurants.splice(i, 1);
+                                    break;
+                                  }
+                                }
+                                // Store updated data in storage.
+                                AsyncStorage.setItem(
+                                  "restaurants",
+                                  JSON.stringify(inRestaurants),
+                                  function () {
+                                    // Set new state to update list.
+                                    this.setState({ listData: inRestaurants });
+                                    // Show toast message to confirm deletion.
+                                    Toast.show({
+                                      text: "Restaurant deleted",
+                                      position: "bottom",
+                                      type: "danger",
+                                      duration: 2000,
+                                    });
+                                  }.bind(this)
+                                );
+                              }.bind(this)
+                            );
+                          },
+                        },
+                        { text: "No" },
+                        { text: "Cancel", style: "cancel" },
+                      ],
+                      { cancelable: true }
+                    );
+                  }}
+                />
+              </View>
+            )}
+          />
+        </View>
+      </Root>
+    );
+  } /* End render(). */
+  /**
+   * Execute after the component mounts.
+   */
+  componentDidMount() {
+    // Block hardware back button on Android.
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      return true;
+    });
+    // Get list of restaurants.
+    AsyncStorage.getItem(
+      "restaurants",
+      function (inError, inRestaurants) {
+        if (inRestaurants === null) {
+          inRestaurants = [];
+        } else {
+          inRestaurants = JSON.parse(inRestaurants);
+        }
+        this.setState({ listData: inRestaurants });
+      }.bind(this)
+    );
+  } /* End componentDidMount() */
+} /* End ListScreen. */
